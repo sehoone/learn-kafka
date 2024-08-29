ecc65912dd48807d07e6e9a21ad48f5bca2448ac
SeHoon <sehoon1989@gmail.com>
2024-08-29 20:57:44 +0900

    feature/init

---
+++ kafka-producer-server/src/main/java/com/sehoon/kafkaproducerserver/KafkaProducerServerApplication.java
@@ -0,0 +1,13 @@
+package com.sehoon.kafkaproducerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerServerApplication.class, args);
+	}
+
+}
---
+++ kafka-producer-server/src/test/java/com/sehoon/kafkaproducerserver/KafkaProducerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehoon.kafkaproducerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
