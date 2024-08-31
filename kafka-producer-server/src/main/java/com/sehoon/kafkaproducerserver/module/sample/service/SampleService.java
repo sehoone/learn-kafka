0184dbe8536e5779a8414bb0dff81138a7af56a0
SeHoon <sehoon1989@gmail.com>
2024-08-31 18:15:54 +0900

    feature/add-send-callback

--- kafka-producer-server/src/main/java/com/sehoon/kafkaproducerserver/module/sample/service/SampleService.java
+++ kafka-producer-server/src/main/java/com/sehoon/kafkaproducerserver/module/sample/service/SampleService.java
@@ -1,6 +1,9 @@
 package com.sehoon.kafkaproducerserver.module.sample.service;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,14 @@ public class SampleService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send(TOPIC, message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(TOPIC, message);
+        
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                System.out.println("Message sent successfully to topic: " + TOPIC);
+            } else {
+                System.err.println("Failed to send message to topic: " + TOPIC);
+            }
+        });
     }
 }
