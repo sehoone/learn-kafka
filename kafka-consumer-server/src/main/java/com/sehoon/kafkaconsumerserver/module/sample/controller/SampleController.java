8e09e1d4ef0f28d40d92093fd23d6042183d1c54
SeHoon <sehoon1989@gmail.com>
2024-09-03 21:23:23 +0900

    feature/add-kafka-admin-client

--- kafka-consumer-server/src/main/java/com/sehoon/kafkaconsumerserver/module/sample/controller/SampleController.java
+++ kafka-consumer-server/src/main/java/com/sehoon/kafkaconsumerserver/module/sample/controller/SampleController.java
@@ -1,23 +1,23 @@
 package com.sehoon.kafkaconsumerserver.module.sample.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sehoon.kafkaconsumerserver.module.sample.service.SampleService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/sample")
 public class SampleController {
     private final SampleService sampleService;
 
-    @PostMapping("/publish")
-    public String publishMessage(@RequestParam("message") String message) {
-        sampleService.sendMessage(message);
-        return "Message published successfully";
+    @GetMapping("/admin-client")
+    public void adminClient() throws Exception {
+        sampleService.getKafkaAdmin();
     }
 }
--- kafka-consumer-server/src/main/java/com/sehoon/kafkaconsumerserver/module/sample/service/SampleService.java
+++ kafka-consumer-server/src/main/java/com/sehoon/kafkaconsumerserver/module/sample/service/SampleService.java
@@ -1,11 +1,29 @@
 package com.sehoon.kafkaconsumerserver.module.sample.service;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.Config;
+import org.apache.kafka.clients.admin.ConfigEntry;
+import org.apache.kafka.clients.admin.DescribeConfigsResult;
+import org.apache.kafka.clients.admin.ListConsumerGroupsResult;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.Node;
+import org.apache.kafka.common.config.ConfigResource;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class SampleService {
@@ -20,5 +38,51 @@ public class SampleService {
     @KafkaListener(topics = TOPIC, groupId = "kafka-consumer-server")
     public void consume(String message) {
         System.out.println("Consumed message: " + message);
+    }
+
+    public void getKafkaAdmin() throws InterruptedException, ExecutionException, Exception {
+        log.info("== Get broker information");
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "15.164.96.99:9092");
+        AdminClient admin = AdminClient.create(configs);
+
+        for (Node node : admin.describeCluster().nodes().get()) {
+            log.info("node : {}", node);
+            ConfigResource cr = new ConfigResource(ConfigResource.Type.BROKER, node.idString());
+            DescribeConfigsResult describeConfigs = admin.describeConfigs(Collections.singleton(cr));
+            describeConfigs.all().get().forEach((broker, config) -> {
+                config.entries().forEach(configEntry -> log.info(configEntry.name() + "= " + configEntry.value()));
+            });
+        }
+
+        log.info("== Get default num.partitions");
+        for (Node node : admin.describeCluster().nodes().get()) {
+            ConfigResource cr = new ConfigResource(ConfigResource.Type.BROKER, node.idString());
+            DescribeConfigsResult describeConfigs = admin.describeConfigs(Collections.singleton(cr));
+            Config config = describeConfigs.all().get().get(cr);
+            Optional<ConfigEntry> optionalConfigEntry = config.entries().stream()
+                    .filter(v -> v.name().equals("num.partitions")).findFirst();
+            ConfigEntry numPartitionConfig = optionalConfigEntry.orElseThrow(Exception::new);
+            log.info("{}", numPartitionConfig.value());
+        }
+
+        log.info("== Topic list");
+        for (TopicListing topicListing : admin.listTopics().listings().get()) {
+            log.info("{}", topicListing.toString());
+        }
+
+        log.info("== test topic information");
+        Map<String, TopicDescription> topicInformation = admin.describeTopics(Collections.singletonList("test-topic2"))
+                .allTopicNames()
+                .get();
+        log.info("{}", topicInformation);
+
+        log.info("== Consumer group list");
+        ListConsumerGroupsResult listConsumerGroups = admin.listConsumerGroups();
+        listConsumerGroups.all().get().forEach(v -> {
+            log.info("{}", v);
+        });
+
+        admin.close();
     }
 }
